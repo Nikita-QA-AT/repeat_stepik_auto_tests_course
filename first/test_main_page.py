b0ac3a5da04ec8d8939eb6d8b149699fc7575507
Nikita-QA-AT <olkin.nikita777@gmail.com>
2025-06-02 11:24:22 +0300

    добавили вспомогательное (2)

--- first/test_main_page.py
+++ first/test_main_page.py
@@ -1,4 +1,11 @@
-
+import pytest
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
